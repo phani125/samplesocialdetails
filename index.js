fe5a6e23842bd6a06df7a423c022468c67ff37f4
phani125 <phanisai125@gmail.com>
2022-10-12 21:01:37 +0530

    make it better

--- index.js
+++ index.js
@@ -1,51 +1,53 @@
-const express = require('express');
-var format = require('date-format');
+const express = require("express");
+const format = require("date-format");
 const app = express();
+
+// swagger docs related
 
 const PORT = process.env.PORT || 4000;
 
-app.get("/", (req,res) => {
-    res.status(200).send("Welcome to user data page");
+app.get("/", (req, res) => {
+  res.status(200).send("<h1>Hello from LCO</h1>");
 });
 
-app.get("/api/v1/:token/*/:tokens", (req,res) =>{
-    res.status(200).json({invalidparams : req.params.token, invaildturl : req.originalUrl });
-} );
+app.get("/api/v1/instagram", (req, res) => {
+  const instaSocial = {
+    username: "Sai",
+    folowers: 6,
+    follows: 7,
+    date: format.asString("dd[MM] - hh:mm:ss", new Date()),
+  };
 
-app.get("/api/v1/:token", (req,res) =>{
-    res.status(200).json({invalidparams : req.params.token, invalidturl : req.originalUrl });
-} )
-
-
-app.get('/api/v1/instagram', (req,res) => {
-
-    const instfol = {
-        name: 'sai',
-        followers: '1',
-        following : '1',
-        date : format.asString("dd-MM-yyyy - hh:mm:ss", new Date())
-    }
-    res.json(instfol)
+  res.status(200).json(instaSocial);
 });
 
-app.get('/api/v1/Linkdin', (req,res) => {
-    const lkdnfol = {
-        name: 'sai',
-        followers: '1',
-        following : '1',
-        date : format.asString('hh:mm:ss', new Date())
-    }
-    res.json(lkdnfol)
+app.get("/api/v1/facebook", (req, res) => {
+  const instaSocial = {
+    username: "Sai",
+    folowers: 880,
+    follows: 10,
+    date: format.asString("dd[MM] - hh:mm:ss", new Date()),
+  };
+
+  res.status(200).json(instaSocial);
 });
 
-app.get('/api/v1/facebook', (req,res) => {
-    const fbfol = {
-        name: 'sai',
-        followers: '1',
-        following : '1',
-        date : format.asString('hh:mm:ss', new Date())
-    }
-    res.json(fbfol)
+app.get("/api/v1/linkedin", (req, res) => {
+  const instaSocial = {
+    username: "hiteshchoudhary",
+    folowers: 80,
+    follows: 1,
+    date: new Date(),
+  };
+
+  res.status(200).json(instaSocial);
 });
 
-app.listen(PORT, ()=> console.log(`App is running at ${PORT}`));
+app.get("/api/v1/:token", (req, res) => {
+  console.log(req.params.token);
+  res.status(200).json({ param: req.params.token });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at ${PORT}`);
+});
